94416964cffe254a996594e1a6643c7fe4961564
HOSSIAN MOHAMMOD ALI <you@example.com>
2021-11-21 14:27:11 +0900

    curcle active not ok

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    $('.banner_slider').slick({
+        
+        dots:true,
+        arrows: true,
+        prevArrow:'<i class="fas fa-arrow-left arrow_left"></i>',
+        nextArrow:'<i class="fas fa-arrow-right arrow_right"></i>',
+    });
+    
+  });

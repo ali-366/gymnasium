e74c9f7b9d769748024462502327dc09db3555af
HOSSIAN MOHAMMOD ALI <you@example.com>
2021-11-24 00:09:40 +0900

    team section ready

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
     $('.banner_slider').slick({
         
         dots:true,
-        arrows: true,
+        arrows: false,
         prevArrow:'<i class="fas fa-arrow-left arrow_left"></i>',
         nextArrow:'<i class="fas fa-arrow-right arrow_right"></i>',
     });
+    $('.venobox').venobox(); 
     
   });

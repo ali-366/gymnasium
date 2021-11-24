0d1d6ec8fdbbbb46c53be073664480f44dccaeb2
HOSSIAN MOHAMMOD ALI <you@example.com>
2021-11-25 00:17:21 +0900

    Zym membership ok

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
     
     $('.banner_slider').slick({
-        
+        autoplay:true,
         dots:true,
         arrows: false,
         prevArrow:'<i class="fas fa-arrow-left arrow_left"></i>',
         nextArrow:'<i class="fas fa-arrow-right arrow_right"></i>',
     });
     $('.venobox').venobox(); 
+
+
+    // testimonial js
+    $('.testi_slider').slick({
+        autoplay:true,
+        dots:true,
+        slidesToShow:2,
+        arrows: false,
+    });
     
   });

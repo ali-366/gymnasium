e91f79e0f52fadff3a80c776637b6af8105099f4
HOSSIAN MOHAMMOD ALI <you@example.com>
2021-11-27 01:10:20 +0900

    98% complite

--- js/custom.js
+++ js/custom.js
@@ -17,5 +17,28 @@ $(document).ready(function(){
         slidesToShow:2,
         arrows: false,
     });
+    // counter up js
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+     // partner js
+     $('.partner_item').slick({
+        autoplay:true,
+        slidesToShow:5,
+        arrows: true,
+        prevArrow:'<i class="fas fa-arrow-left arrow_left"></i>',
+        nextArrow:'<i class="fas fa-arrow-right arrow_right"></i>',
+    });
+
+    // for shedule js
+    var mixer = mixitup('.sheduleAll');
+    
+    
+    
+    // activee class
+    $('.shedule_menu ul li').on('click',function(){
+        $(this).addClass('active_menu').siblings().removeClass('active_menu');
+    });
     
   });
